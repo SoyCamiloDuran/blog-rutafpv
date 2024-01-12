6e4d56f51c3dbcd0d6a27e82de64b9012eb51034
Camilo Duran <durancamilo11@gmail.com>
2024-01-12 02:45:16 -0300

    nose que hago

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import dj_database_url
 import os
 import environ
 
@@ -117,6 +118,11 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+DATABASES = {
+	"default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
+
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 
 CORS_ORIGIN_WHITELIST = [
