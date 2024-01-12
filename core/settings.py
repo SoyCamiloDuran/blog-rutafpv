1a26cdfa00ac971d91eab6f24fe9e7f698689e0b
Camilo Duran <durancamilo11@gmail.com>
2024-01-12 02:17:16 -0300

    new build and static

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ DOMAIN = 'http://localhost:8000'
 SITE_NAME = 'RutaFPV'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
