b62cd692fe180bbd5bdc1a38bd63a062f1b0e00b
Camilo Duran <durancamilo11@gmail.com>
2024-01-12 02:15:23 -0300

    degug true

--- core/settings.py
+++ core/settings.py
@@ -17,7 +17,7 @@ DOMAIN = 'http://localhost:8000'
 SITE_NAME = 'RutaFPV'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
