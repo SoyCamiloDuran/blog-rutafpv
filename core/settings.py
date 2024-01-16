f5788dcd99896038eb938fb039c252c97fadd6d3
Camilo Duran <durancamilo11@gmail.com>
2024-01-15 23:27:11 -0300

    new two + debug false

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ DOMAIN = 'http://localhost:8000'
 SITE_NAME = 'RutaFPV'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 # DEBUG = True
 
 ALLOWED_HOSTS = [
